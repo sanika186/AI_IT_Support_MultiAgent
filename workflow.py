1ddff8fe626ca82ab1a337216aa57ac9b0ebce9d
Sanika Kulkarni <sanikakulkarni9699@gmail.com>
2026-07-21 18:15:35 +0530

    Add files via upload

---
+++ workflow.py
@@ -0,0 +1,60 @@
+from typing import TypedDict
+
+from langgraph.graph import StateGraph, END
+
+from agents import classify_issue, generate_solution
+from utils import get_issue_details
+
+
+class ITSupportState(TypedDict):
+    user_issue: str
+    uploaded_image: object
+    category: str
+    issue_details: dict
+    final_response: str
+
+
+# -----------------------------
+# Agent 1 : Issue Classifier
+# -----------------------------
+def classify_node(state: ITSupportState):
+
+    issue = state["user_issue"]
+    category = classify_issue(issue)
+
+    return {
+        "category": category,
+        "user_issue": issue
+    }
+# -----------------------------
+# Agent 2 : Solution Generator
+# -----------------------------
+def solution_node(state: ITSupportState):
+
+    details = get_issue_details(state["category"])
+
+    final_response = generate_solution(
+        state["category"],
+        details
+    )
+
+    return {
+        "issue_details": details,
+        "final_response": final_response
+    }
+
+
+# -----------------------------
+# Build LangGraph Workflow
+# -----------------------------
+builder = StateGraph(ITSupportState)
+
+builder.add_node("classifier", classify_node)
+builder.add_node("solution", solution_node)
+
+builder.set_entry_point("classifier")
+
+builder.add_edge("classifier", "solution")
+builder.add_edge("solution", END)
+
+workflow = builder.compile()
